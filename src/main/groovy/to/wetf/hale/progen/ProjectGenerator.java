ce5eced80257b90c942e1bb6070b5cf417e950b6
Simon Templer <simon@wetransform.to>
2018-08-06 10:29:41 +0200

    fix strange Java error running tests

--- src/main/groovy/to/wetf/hale/progen/ProjectGenerator.java
+++ src/main/groovy/to/wetf/hale/progen/ProjectGenerator.java
@@ -6,31 +6,31 @@ import java.io.OutputStream;
 import to.wetf.hale.progen.impl.ProjectGeneratorImpl;
 
 public interface ProjectGenerator {
-  
+
   /**
    * Create a default project generator instance.
-   * 
+   *
    * @return a new project generator instance
    */
   static ProjectGenerator create() {
     return new ProjectGeneratorImpl();
   }
-  
+
   /**
    * Generate a HALE project based on a provided target XML Schema.
-   * 
-   * @param outProject the output stream to write the generated project to 
+   *
+   * @param outProject the output stream to write the generated project to
    * @param inTargetXSD the input stream with the content of the single XSD file,
    *   the XSD depend on external resources, if they are generally accessible
    * @param config the project configuration with various settings for project
    *   generation
    */
   public void generateTargetXSDProject(OutputStream outProject, InputStream inTargetXSD, ProjectConfiguration config);
-  
+
   /**
    * Generate a HALE project based on a set of target XML Schemas.
-   * 
-   * @param outProject the output stream to write the generated project to 
+   *
+   * @param outProject the output stream to write the generated project to
    * @param targetXSDs descriptors of target XML Schemas at public locations,
    *   if there are multiple, the namespace and namespace prefix for each descriptor
    *   is mandatory
--- src/test/groovy/to/wetf/hale/progen/ProjectGeneratorTest.groovy
+++ src/test/groovy/to/wetf/hale/progen/ProjectGeneratorTest.groovy
@@ -2,12 +2,16 @@ package to.wetf.hale.progen
 
 import javax.xml.namespace.QName
 import org.junit.Test
+
+import to.wetf.hale.progen.impl.ProjectGeneratorImpl
+
 import static org.junit.Assert.*
 
+
 class ProjectGeneratorTest {
-  
+
   private static final boolean DELETE_TEST_PROJECTS = true
-  
+
   @Test
   void testSchemaProvided() {
     URL schemaUrl = getClass().getClassLoader().getResource("schema/multischema1.xsd")
@@ -16,7 +20,7 @@ class ProjectGeneratorTest {
     if (DELETE_TEST_PROJECTS) {
       projectFile.deleteOnExit()
     }
-    
+
     // prepare list of relevant target types
     String NS_EF = 'http://inspire.ec.europa.eu/schemas/ef/4.0'
     String NS_AM = 'http://inspire.ec.europa.eu/schemas/am/4.0'
@@ -25,84 +29,84 @@ class ProjectGeneratorTest {
       new QName(NS_EF, 'EnvironmentalMonitoringFacilityType'),
       new QName(NS_AM, 'ManagementRestrictionOrRegulationZoneType')
     ]
-    
+
     // prepare project configuration
     ProjectConfiguration config = new ProjectConfiguration()
     config.relevantTargetTypes = targetTypes
-    
+
     projectFile.withOutputStream { outProject ->
       schemaUrl.openStream().withStream { inTargetXSD ->
-        
+
         // create project generator
-        ProjectGenerator gen = ProjectGenerator.create()
-        
+        ProjectGenerator gen = new ProjectGeneratorImpl()
+
         // run project generation
         gen.generateTargetXSDProject(outProject, inTargetXSD, config)
-        
+
       }
     }
-    
+
     println "Generated HALE project file ${projectFile.absolutePath}"
-    
+
     assertTrue(projectFile.exists())
     assertTrue(projectFile.size() > 0)
-    
+
     //TODO test generated project in some way?
-    
+
   }
-  
+
   @Test
   void testInspireSingle() {
     File projectFile = File.createTempFile('progen', '.halez')
     if (DELETE_TEST_PROJECTS) {
       projectFile.deleteOnExit()
     }
-    
+
     // prepare list of relevant target types
     String NS_HYP = 'http://inspire.ec.europa.eu/schemas/hy-p/4.0'
     List<QName> targetTypes = [
       new QName(NS_HYP, 'WatercourseType'),
       new QName(NS_HYP, 'StandingWaterType')
     ]
-    
+
     // prepare list of schemas
     List<SchemaDescriptor> schemas = []
     schemas << new SchemaDescriptor(
       location: URI.create('http://inspire.ec.europa.eu/schemas/hy-p/4.0/HydroPhysicalWaters.xsd'),
       namespace: NS_HYP,
       namespacePrefix: 'hy-p')
-    
+
     // prepare project configuration
     ProjectConfiguration config = new ProjectConfiguration()
     config.projectName = 'Map to INSPIRE HydroPhysicalWaters'
     config.relevantTargetTypes = targetTypes
-    
+
     projectFile.withOutputStream { outProject ->
-        
+
       // create project generator
-      ProjectGenerator gen = ProjectGenerator.create()
-      
+      ProjectGenerator gen = new ProjectGeneratorImpl()
+
       // run project generation
       gen.generateTargetXSDProject(outProject, schemas, config)
 
     }
-    
+
     println "Generated HALE project file ${projectFile.absolutePath}"
-    
+
     assertTrue(projectFile.exists())
     assertTrue(projectFile.size() > 0)
-    
+
     //TODO test generated project in some way?
-    
+
   }
-  
+
   @Test
   void testInspireMulti() {
     File projectFile = File.createTempFile('progen', '.halez')
     if (DELETE_TEST_PROJECTS) {
       projectFile.deleteOnExit()
     }
-    
+
     // prepare list of relevant target types
     String NS_HYP = 'http://inspire.ec.europa.eu/schemas/hy-p/4.0'
     String NS_HYN = 'http://inspire.ec.europa.eu/schemas/hy-n/4.0'
@@ -111,7 +115,7 @@ class ProjectGeneratorTest {
       new QName(NS_HYP, 'StandingWaterType'),
       new QName(NS_HYN, 'WatercourseLinkType')
     ]
-    
+
     // prepare list of schemas
     List<SchemaDescriptor> schemas = []
     schemas << new SchemaDescriptor(
@@ -122,28 +126,28 @@ class ProjectGeneratorTest {
       location: URI.create('http://inspire.ec.europa.eu/schemas/hy-n/4.0/HydroNetwork.xsd'),
       namespace: NS_HYN,
       namespacePrefix: 'hy-n')
-    
+
     // prepare project configuration
     ProjectConfiguration config = new ProjectConfiguration()
     config.relevantTargetTypes = targetTypes
-    
+
     projectFile.withOutputStream { outProject ->
-        
+
       // create project generator
-      ProjectGenerator gen = ProjectGenerator.create()
-      
+      ProjectGenerator gen = new ProjectGeneratorImpl()
+
       // run project generation
       gen.generateTargetXSDProject(outProject, schemas, config)
 
     }
-    
+
     println "Generated HALE project file ${projectFile.absolutePath}"
-    
+
     assertTrue(projectFile.exists())
     assertTrue(projectFile.size() > 0)
-    
+
     //TODO test generated project in some way?
-    
+
   }
 
 }
