883f0d57fc5bb2a2138f457533899e48a2674102
Simon Templer <simon@wetransform.to>
2018-08-06 14:38:52 +0200

    add missing file headers, fix file location

--- src/main/groovy/to/wetf/hale/progen/impl/WriteOnceOutputSupplier.java
+++ src/main/groovy/to/wetf/hale/progen/impl/WriteOnceOutputSupplier.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 wetransform GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package to.wetf.hale.progen.impl;
 
 import java.io.IOException;
@@ -9,7 +25,7 @@ import eu.esdihumboldt.hale.common.core.io.supplier.LocatableOutputSupplier;
 public class WriteOnceOutputSupplier implements LocatableOutputSupplier<OutputStream> {
 
   private final OutputStream out;
-  
+
   public WriteOnceOutputSupplier(OutputStream out) {
     super();
     this.out = out;
